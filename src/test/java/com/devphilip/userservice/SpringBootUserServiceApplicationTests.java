625da29481b679c38a5b7d8a718a537fb6cc06c2
Philip Akpeki <philipakpeki@gmail.com>
2021-07-30 23:38:57 +0100

    add all initial files

---
+++ src/test/java/com/devphilip/userservice/SpringBootUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devphilip.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
